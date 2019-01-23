061025af71903f06f51629cf30ede5d38408c561
Alejandro Di Battista <alejandrodibattista@gmail.com>
2019-01-23 13:51:04 -0300

    Solid

---
+++ Ejercicios01/Ejercicios01/EjemploLinq.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Ejercicios04
+{
+	static class EjemploLinq
+	{
+		public class Persona
+		{
+			public string Nombre { get; set; }
+			public int Edad { get; set; }
+			public bool Hombre { get; set; }
+			public override string ToString() => $"Nombre: {Nombre,-10} Edad: {Edad,2} Sexo: {( Hombre ? "H" : "M" ) }";
+		}
+
+		static void Mostrar<T>( this IEnumerable<T> lista, string mensaje = "" )
+		{
+			Console.WriteLine( $" > {mensaje} " );
+			foreach( var item in lista ) {
+				Console.WriteLine( $" · { item }" );
+			}
+			Console.WriteLine();
+		}
+
+
+
+		static void Main( string[] args )
+		{
+			Console.WriteLine( "DEMOSTRACION LinQ" );
+
+			var salida = from p in TraerPersonas() where p.Hombre && p.Edad < 50 select new { p.Nombre, p.Edad };
+			foreach( var p in salida ) {
+				Console.WriteLine( p );
+			}
+			Console.ReadLine();
+		}
+
+		static IEnumerable<Persona> TraerPersonas()
+		{
+			yield return new Persona() { Nombre = "Alejandro", Edad = 50, Hombre = true };
+			yield return new Persona() { Nombre = "Juan", Edad = 40, Hombre = true };
+			yield return new Persona() { Nombre = "Jose", Edad = 30, Hombre = true };
+			yield return new Persona() { Nombre = "Alejandra", Edad = 30, Hombre = false };
+			yield return new Persona() { Nombre = "Juana", Edad = 40, Hombre = false };
+			yield return new Persona() { Nombre = "Josefa", Edad = 50, Hombre = false };
+			yield return new Persona() { Nombre = "Mario", Edad = 20, Hombre = true };
+			yield return new Persona() { Nombre = "Maria", Edad = 10, Hombre = false };
+		}
+	}
+}
---
+++ SOLID-SRP/SOLID-SRP/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solid
+{
+	namespace SRP
+	{
+		class Persona
+		{
+			public string Nombre { get; set; }
+			public string Apellido { get; set; }
+		}
+
+
+		class DemoSRP
+		{
+			static void Main( string[] args )
+			{
+				Console.WriteLine( "Alta de usuario en Amasin!" );
+
+				Persona p = new Persona();
+
+				Console.Write( "¿Cúal es el nombre? >" );
+				p.Nombre = Console.ReadLine();
+
+				Console.Write( "¿Cúal es el Apellido? >" );
+				p.Apellido = Console.ReadLine();
+
+				if( string.IsNullOrWhiteSpace( p.Nombre ) ) {
+					Console.WriteLine( "Debe ingresar un Nombre valido" );
+					Console.ReadLine();
+					return;
+				}
+
+				if( string.IsNullOrWhiteSpace( p.Apellido ) ) {
+					Console.WriteLine( "Debe ingresar un Nombre Apellido" );
+					Console.ReadLine();
+					return;
+				}
+
+				Console.WriteLine( $"El nombre de usuario es {p.Nombre.Substring( 0, 1 )}{p.Apellido.Replace(" ", "")}" );
+				Console.ReadLine();
+			}
+		}
+	}
+}
