6af1904eb61c0f1180ac6508dee4501078c088d6
Alejandro Di Battista <alejandrodibattista@gmail.com>
2019-02-21 14:32:52 -0300

    Filtrador

---
+++ Ejercicios01/Ejercicios01/16 Ordenador.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicios16
+{
+	static class Ordenador
+	{
+
+		public class Persona 
+		{
+			public string Nombre { get; set; }
+			public string Apellido { get; set; }
+			public int Edad { get; set; }
+
+		}
+
+		public class Comparador
+		{
+			public int Comparar( Persona a, Persona b )
+			{
+				var comparar = 0;
+
+				if( comparar == 0 ) comparar = a.Apellido.CompareTo( b.Apellido );
+				if( comparar == 0 ) comparar = a.Nombre.CompareTo( b.Nombre );
+				if( comparar == 0 ) comparar = -a.Edad.CompareTo( b.Edad );
+
+				return comparar;
+			}
+		}
+
+
+		static List<Persona> Ordenar( IEnumerable<Persona> datos, Comparador c ) 
+		{
+			var lista = new List<Persona>( datos );
+
+			for( var i = 0; i < lista.Count - 1; i++ ) {
+				for( var j = i + 1; j < lista.Count; j++ ) {
+					if(  c.Comparar(lista[ j ], lista[ i ]) < 0 ) {
+						var tmp = lista[ i ];
+						lista[ i ] = lista[ j ];
+						lista[ j ] = tmp;
+					}
+				}
+			}
+			return lista;
+		}
+
+
+		static void Main( string[] args )
+		{
+			var datos = new List<Persona> {
+				new Persona{ Nombre = "Alejandro", Apellido = "Di Battista", Edad = 51 },
+				new Persona{ Nombre = "Mirta",     Apellido = "Peroña",      Edad = 72 },
+				new Persona{ Nombre = "Franco",    Apellido = "Di Battista", Edad = 14 },
+				new Persona{ Nombre = "Mirta",     Apellido = "Peroña",      Edad = 50 },
+				new Persona{ Nombre = "Maira",     Apellido = "Di Battista", Edad = 19 },
+			};
+
+			var c = new Comparador();
+
+			Ordenar( datos, c ).ForEach( Console.WriteLine );
+			Console.WriteLine();
+			Ordenar( datos, c ).ForEach( Console.WriteLine );
+			Console.WriteLine(Sumar(1,2));
+			var s = new Sumador( 1, 2 );
+			Console.WriteLine( s.Sumar() );
+
+			Console.ReadLine();
+		}
+		public static int Sumar( int a, int b ) => a + b;
+		class Sumador
+		{
+			public int A { get; set; }
+			public int B{ get; set; }
+
+			public Sumador(int a, int b )
+			{
+				A = a;
+				B = b;
+			}
+			public int Sumar() => A + B;
+
+		}
+	}
+}
---
+++ Ejercicios01/Ejercicios01/17 Filtrador.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicios17
+{
+	static class Filtrador
+	{
+		public class Persona
+		{
+			public string Nombre { get; set; }
+			public string Apellido { get; set; }
+			public int Edad { get; set; }
+			public bool EsHombre { get; set; }
+			public override string ToString() => $"{Apellido}, {Nombre} ({Edad} {( EsHombre ? "H" : "M" )})";
+		}
+
+		static List<Persona> datos = new List<Persona> {
+			new Persona{ Nombre = "Alejandro", Apellido = "Di Battista", Edad = 51, EsHombre = true  },
+			new Persona{ Nombre = "Mirta",     Apellido = "Peroña",      Edad = 72, EsHombre = false },
+			new Persona{ Nombre = "Franco",    Apellido = "Di Battista", Edad = 14, EsHombre = true  },
+			new Persona{ Nombre = "Mirta",     Apellido = "Peroña",      Edad = 50, EsHombre = false },
+			new Persona{ Nombre = "Maira",     Apellido = "Di Battista", Edad = 15, EsHombre = false },
+		};
+
+		interface IFiltro
+		{
+			bool Condicion( Persona p );
+		}
+
+		class Hombre : IFiltro
+		{
+			public bool Condicion( Persona p ) => p.EsHombre;
+		}
+
+		class Contiene : IFiltro
+		{
+			private string Texto;
+			public Contiene( string texto ) => Texto = texto;
+			public bool Condicion( Persona p ) => p.Apellido.Contains( Texto ) || p.Nombre.Contains( Texto );
+		}
+
+		class Invertir : IFiltro
+		{
+			private IFiltro Anterior;
+			public Invertir( IFiltro anterior ) => Anterior = anterior;
+			public bool Condicion( Persona p ) => !Anterior.Condicion( p );
+		}
+
+		class Y : IFiltro
+		{
+			private IFiltro A, B;
+			public Y( IFiltro a, IFiltro b )
+			{
+				A = a;
+				B = b;
+			}
+			public bool Condicion( Persona p ) => A.Condicion( p ) && B.Condicion( p );
+		}
+		class O : IFiltro
+		{
+			private IFiltro A, B;
+			public O( IFiltro a, IFiltro b )
+			{
+				A = a;
+				B = b;
+			}
+			public bool Condicion( Persona p ) => A.Condicion( p ) || B.Condicion( p );
+		}
+
+		class MenorA : IFiltro
+		{
+			int Edad;
+			public MenorA( int edad ) => Edad = edad;
+			public bool Condicion( Persona p ) => p.Edad < Edad;
+		}
+
+		class Menor : MenorA
+		{
+			public Menor() : base( 18 ) { }
+			public bool Condicion( Persona p ) => p.Edad < 18;
+		}
+
+		class Mayor : IFiltro
+		{
+			IFiltro Base;
+			public Mayor() => Base = new Invertir( new Menor() );
+			public bool Condicion( Persona p ) => Base.Condicion( p );
+		}
+		static IEnumerable<Persona> Filtrar( this IEnumerable<Persona> personas, IFiltro filtro )
+		{
+			foreach( var persona in personas ) {
+				if( filtro.Condicion( persona ) )
+					yield return persona;
+			}
+		}
+		static void Main( string[] args )
+		{
+			Console.WriteLine( "LISTADO DE PERSONAS" );
+			IFiltro hombre = new Hombre();
+			IFiltro mujer = new Invertir( hombre );
+
+			IFiltro condicion = new O( new Y( hombre, new Menor() ), new Y( mujer, new Mayor() ) );
+
+			//f = new Y( new Menor(), f );
+			foreach( var persona in Filtrar( datos, condicion ) ) {
+				Console.WriteLine( $"· {persona}" );
+			}
+			Console.ReadLine();
+		}
+	}
+}
