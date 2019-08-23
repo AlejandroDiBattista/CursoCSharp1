b9ccdad393a882087474d7cbabe53ea72967de9e
Alejandro Di Battista <alejandrodibattista@gmail.com>
2019-08-23 17:17:00 -0300

    Pruebas Rabbit

---
+++ PruebaRabbit/HerenciaEquals.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prueba2 {
+    public abstract class Padre {
+        public string A { get; set; }
+        public string B { get; set; }
+        
+
+        protected bool Equals(Padre other)
+        {
+            return Equals(A, other?.A) && object.Equals(B, other?.B);
+        }
+
+        public override bool Equals(object obj) {
+            if(ReferenceEquals(null, obj)) return false;
+            if(ReferenceEquals(this, obj)) return true;
+            if(obj.GetType() != GetType()) return false;
+            return Equals(obj as Padre);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (A != null ? A.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (B != null ? B.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+    }
+
+
+    public class Hijo : Padre { }
+
+    public class Hija : Padre, IEquatable<Hija> {
+        public bool Equals(Hija other) {
+            return Equals(A, other?.A) && object.Equals(B, other?.B);
+        }
+        
+    }
+    
+
+    public class Nieto : Hijo { }
+
+    public class Nieta : Padre, IEquatable<Nieta> {
+        public bool Equals(Nieta other) {
+            return Equals(A, other?.A) && object.Equals(B, other?.B);
+        }
+    }
+    
+
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" » Prueba herencia en abstract");
+
+            var a = new Hija() { A = "Hola", B = "Chau" };
+            object b = new Hija() { A = "Hola", B = "Chau" };
+
+            Console.WriteLine($" · a.Equals(b)    => {a.Equals(b)}");
+            Console.WriteLine($" · b.Equals(a)    => {b.Equals(a)}");
+            Console.WriteLine($" · a.Equals(null) => {a.Equals(null)}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PruebaRabbit/ImplementacionEquals.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Dynamic;
+using System.Text.RegularExpressions;
+
+namespace Prueba1
+{
+    class Program
+    {
+        public class Punto :IEquatable<Punto>
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+
+            public override bool Equals(object obj) => Equals(obj as Punto);
+            public bool Equals(Punto other) => !ReferenceEquals(other, null) && (X, Y) == (other.X, other.Y);
+            public override int GetHashCode() => (X, Y).GetHashCode();
+
+            static public bool operator ==(Punto a, Punto b) =>  a?.Equals(b) ?? false;
+            static public bool operator !=(Punto a, Punto b) => !a?.Equals(b) ?? true;
+        }
+
+        public static dynamic Copia { get; set; } = new ExpandoObject();
+
+        public static void Copiar(Punto p)
+        {
+            Copia = p;
+        }
+
+        static void PruebaCopiarEnExpandoObject()
+        {
+            var p = new Punto { X = 10, Y = 20 };
+            Copiar(p);
+            Console.WriteLine(Copia.ToString());
+            Console.WriteLine($"Copia > X:{Copia.X} Y:{Copia.Y}");
+            Console.WriteLine($"P     > X:{p.X} Y:{p.Y}");
+            Copia.X = 100;
+            Console.WriteLine($"Copia > X:{Copia.X} Y:{Copia.Y}");
+            Console.WriteLine($"P     > X:{p.X} Y:{p.Y}");
+            p = new Punto { X = 1000, Y = 1000 };
+            Console.WriteLine($"Copia > X:{Copia.X} Y:{Copia.Y}");
+            Console.WriteLine($"P     > X:{p.X} Y:{p.Y}");
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            var a = new Punto{ X = 10, Y = 10};
+            object b = new Punto { X = 10, Y = 10 };
+            var c = new Punto { X = 20, Y = 20 };
+
+            Console.WriteLine(" » Prueba Implementación Equals");
+            Console.WriteLine($" · A eq B     > {a.Equals(b)}");
+            Console.WriteLine($" · A eq C     > {a.Equals(c)}");
+            Console.WriteLine($" · A eq ERROR > {a.Equals("Hola")}");
+            Console.WriteLine($" · A eq NULL  > {a.Equals(null)}");
+            Console.WriteLine($" · A == NULL  > {a == null}");
+            Console.WriteLine($" · (A) == (A) > {(a) == (a)}");
+            Console.WriteLine($" · (A) == (B) > {(a) == (b)}");
+            Console.WriteLine($" · (A) eq (B) > {(a).Equals(b)}");
+            Console.ReadLine();
+
+        }
+    }
+}
