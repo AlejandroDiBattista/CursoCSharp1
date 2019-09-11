d6464c20035fee9b8786adb26166916e96f2480d
Alejandro Di Battista <alejandrodibattista@gmail.com>
2019-09-11 18:05:14 -0300

    .

--- PruebaRabbit/PruebaInstanciaInterface.cs
+++ PruebaRabbit/PruebaInstanciaInterface.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace PruebaRabbit {
     interface IPersona {
@@ -22,15 +23,34 @@ namespace PruebaRabbit {
 
     class Programa { 
         static void Main(string[] args) {
-            Console.WriteLine(" > Prueba Instancia Interface");
-            var a = new IPersona[] { new Persona("Ale", 52) };
-            foreach(var p in a) {
-                Console.WriteLine(p);
+            var a = new[]{
+                new[]{ 1, 2, 3 },
+                new[]{ 4, 5, 6 }
+            };
+
+            var b = a.SelectMany(x => x).ToArray();
+
+            foreach(var x in a) {
+                Console.WriteLine(">");
+                foreach(var y in x)
+                    Console.WriteLine(y);
             }
-            Console.WriteLine(a.GetType().Name);
-            Console.WriteLine(a.Length);
-            //Console.WriteLine(a[2]);
+
+            Console.WriteLine("------");
+            foreach(var z in b)
+                Console.WriteLine(z);
+
             Console.ReadLine();
+
+            //Console.WriteLine(" > Prueba Instancia Interface");
+            //var a = new IPersona[] { new Persona("Ale", 52) };
+            //foreach(var p in a) {
+            //    Console.WriteLine(p);
+            //}
+            //Console.WriteLine(a.GetType().Name);
+            //Console.WriteLine(a.Length);
+            ////Console.WriteLine(a[2]);
+            //Console.ReadLine();
         }
     }
 }
